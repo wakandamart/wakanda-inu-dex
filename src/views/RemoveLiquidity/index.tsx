5a96b9d7b39f96a354d977c97d2a4adc562972fc
AjayiMike <ajayimikeofficial@gmail.com>
2022-08-19 10:59:09 +0100

    change LP token detais for signature request of liquidity removal

--- src/views/RemoveLiquidity/index.tsx
+++ src/views/RemoveLiquidity/index.tsx
@@ -168,7 +168,7 @@ export default function RemoveLiquidity() {
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Wakanda LP',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
